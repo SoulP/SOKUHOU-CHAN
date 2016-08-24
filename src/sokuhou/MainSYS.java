aeb158f2cd124cde8e1b4e9815408f0516550d28
SoulP <SoulP@KBC11403180>
2016-08-24 16:19:29 +0900

    wsyws

--- src/sokuhou/MainSYS.java
+++ src/sokuhou/MainSYS.java
@@ -3,8 +3,8 @@ package sokuhou;
 public class MainSYS {
 
 	public static void main(String[] args) {
-		// TODO 閾ｪ蜍慕函謌舌＆繧後◆繝｡繧ｽ繝�繝峨�ｻ繧ｹ繧ｿ繝�
-		System.out.println("1111awdada1");
+		// TODO あいうえ
+		System.out.println("	e2q1");
 	//sys
 		//YAHOO!
 		//gbvhgjn

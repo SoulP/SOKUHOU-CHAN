8b7aa6d2efefe7946b5ad6b1d43bdcb802160a4c
user <user@kbc11403104.mshome.net>
2016-08-24 16:17:59 +0900

    ｈｋｃｖｊｃ

--- src/sokuhou/MainSYS.java
+++ src/sokuhou/MainSYS.java
@@ -3,10 +3,11 @@ package sokuhou;
 public class MainSYS {
 
 	public static void main(String[] args) {
-		// TODO 自動生成されたメソッド・スタブ
+		// TODO 閾ｪ蜍慕函謌舌＆繧後◆繝｡繧ｽ繝�繝峨�ｻ繧ｹ繧ｿ繝�
 		System.out.println("1111awdada1");
 	//sys
 		//YAHOO!
+		//gbvhgjn
 	}
 	//deswe
 

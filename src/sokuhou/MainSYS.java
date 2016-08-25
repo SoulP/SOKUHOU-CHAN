152ec64534cae2f6d771f33c6c6e0039088dc75f
SoulP <SoulP@KBC11403180>
2016-08-25 11:29:13 +0900

    aaa

--- src/sokuhou/MainSYS.java
+++ src/sokuhou/MainSYS.java
@@ -3,12 +3,7 @@ package sokuhou;
 public class MainSYS {
 
 	public static void main(String[] args) {
-		// TODO 生麦生米生卵
-		System.out.println("	2q1e");
-	//sys
-		//YAHOO!
-		//gbvhgjn
+		// メインシステム
+		System.out.println("AAAA");
 	}
-	//deswe
-
 }

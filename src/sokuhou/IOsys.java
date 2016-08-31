3dfd253a9fcdb69a542e51cae297174008d960f8
user <user@kbc11403013>
2016-08-31 13:13:11 +0900

    aaa

--- src/sokuhou/IOsys.java
+++ src/sokuhou/IOsys.java
@@ -3,12 +3,8 @@ package sokuhou;
 public class IOsys extends IO{
 	// Input / Output (入出力)
 
-<<<<<<< HEAD
 	public void run(){
 
 	}
 }
-=======
 
-}
->>>>>>> branch 'Head' of https://github.com/SoulP/SOKUHOU-CHAN.git

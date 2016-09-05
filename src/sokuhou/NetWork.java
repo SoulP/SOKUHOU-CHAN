e3489f2adbf6361d4bf69ced2aaeb8f8bea8ddcd
user <user@kbc11403149>
2016-09-05 15:10:11 +0900

    aa

--- src/sokuhou/NetWork.java
+++ src/sokuhou/NetWork.java
@@ -1,6 +1,8 @@
 package sokuhou;
 
 import java.util.ArrayList;
+import java.io.*;
+import java.net.*;
 
 public abstract class NetWork extends Net{
 	// ネットワーク
@@ -32,7 +34,46 @@ public abstract class NetWork extends Net{
 		System.out.println("ニーハオ");//ニーハオと出力
 
 		}
+	/** Httpプロトコルによるソース表示（HttpURLConnectionクラス利用） **/
+	public class HttpUrl {
+	  public void main(String args[]) {
+	    String url = "http://ash.jp/";
+	    String proxyHost = "";
+	    int    proxyPort = 8080;
+
+	    URL urlObj;
+	    HttpURLConnection urlCon;
+	    BufferedReader urlIn;
+	    String str;
+
+	    if (args.length > 0) {
+	      url = args[0];
+	    }
+	//  System.out.println("URL: " + url);
+
+	    try {
+	      urlObj = new URL("http", proxyHost, proxyPort, url);
+
+	      // URL接続
+	      urlCon = (HttpURLConnection)urlObj.openConnection();
+	      urlCon.setRequestMethod("GET");
+	      urlIn = new BufferedReader(new InputStreamReader(urlCon.getInputStream()));
+
+	      // HTMLソースの表示
+	      while ((str = urlIn.readLine()) != null) {
+	        System.out.println(str);
+	      }
+
+	      // URL切断
+	      urlIn.close();
+	      urlCon.disconnect();
+
+	    } catch (Exception ex) {
+	      ex.printStackTrace();
+	    }
+	  }
 	}
+}
 
 
 

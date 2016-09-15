903ac7d4f6aa88510debbe80d98d7d3f4841d6fa
Taihei Yoshimoto <taihei_y-14a@stu.kbc.ac.jp>
2016-09-15 12:08:27 +0900

    JFrame処理追加

--- src/sokuhou/IO.java
+++ src/sokuhou/IO.java
@@ -2,5 +2,4 @@ package sokuhou;
 
 public abstract class IO extends Thread{
 
-	public abstract void run();
 }
--- src/sokuhou/IOsys.java
+++ src/sokuhou/IOsys.java
@@ -3,8 +3,6 @@ package sokuhou;
 public class IOsys extends IO{
 	// Input / Output (入出力)
 
-	public void run(){
 
-	}
 }
 

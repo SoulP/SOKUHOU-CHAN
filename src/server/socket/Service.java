0f94a3a9e81ed991ced953dd115c9ca4348bb227
Taihei Yoshimoto <taihei_y-14a@stu.kbc.ac.jp>
2017-01-12 10:32:16 +0900

    通信ソケット改善とウォッチドッグタイマ作成

--- src/server/socket/Service.java
+++ src/server/socket/Service.java
@@ -3,9 +3,23 @@ package server.socket;
 import java.net.Socket;
 
 public class Service extends Thread{
-	private Socket socket;
+	private final Socket socket;
+	public String clientIP;// クライアントのIPアドレス
 
-	public Service(Socket socket){
+
+	public Service(final Socket socket){
 		this.socket = socket;
+	}
+
+	public void run(){
+		WatchDogTimer wdt = new WatchDogTimer(socket);
+		wdt.start();
+
+		try{
+
+		}catch (Exception e){
+			System.out.println(e);
+			e.printStackTrace();
+		}
 	}
 }
---
+++ src/server/socket/WatchDogTimer.java
@@ -0,0 +1,127 @@
+package server.socket;
+
+import java.net.Socket;
+
+public class WatchDogTimer extends JSocket{
+	private int interval;// 間隔
+	private int afterTime;// 予測経過時間
+	private int nowTime;// 現在の時間
+	private boolean rtm;// 時間監視
+
+	// コンストラクタ
+	public WatchDogTimer(){
+		// 初期設定
+		setSocket(null);// null値
+		interval = 10000;// 10秒の間隔(ミリ秒単位)
+		rtm = true;// 時間監視をtrueに設定
+	}
+
+	// コンストラクタ
+	public WatchDogTimer(Socket socket){
+		// 初期設定
+		setSocket(socket);// ソケット 入力
+		try {
+			open();
+		} catch (Exception e) {
+			System.out.println(e);
+			e.printStackTrace();
+			try {
+				close();
+			} catch (Exception e1) {
+				System.out.println(e1);
+				e1.printStackTrace();
+				setDOS(null);// null値
+				setDIS(null);// null値
+			}
+		}
+		interval = 10000;// 10秒の間隔(ミリ秒単位)
+		rtm = true;// 時間監視をtrueに設定
+	}
+
+	// コンストラクタ
+	public WatchDogTimer(int interval){
+		// 初期設定
+		setSocket(null);// null値
+		this.interval = interval;// 間隔設定
+		rtm = true;// 時間監視をtrueに設定
+	}
+
+	// コンストラクタ
+	public WatchDogTimer(Socket socket, int interval){
+		// 初期設定
+		setSocket(socket);// ソケット 入力
+		try {
+			open();
+		} catch (Exception e) {
+			System.out.println(e);
+			e.printStackTrace();
+			try {
+				close();
+			} catch (Exception e1) {
+				System.out.println(e1);
+				e1.printStackTrace();
+				setDOS(null);// null値
+				setDIS(null);// null値
+			}
+		}
+		this.interval = interval;// 間隔設定
+		rtm = true;// 時間監視をtrueに設定
+	}
+
+	public void run(){
+		rtm = true;// 時間監視をtrueに設定
+		nowTime = afterTime = (int) System.currentTimeMillis();// 現在の時間(ミリ秒単位)取得
+		afterTime += interval;// 間隔を足す
+
+		try{
+			do{
+				if(nowTime >= afterTime){// 現在の時間が予定以上過ぎた場合
+					afterTime = nowTime;// 現在の時間をコピー
+					afterTime += interval;// 間隔を足す
+					getDOS().writeBoolean(false);// クライアントにfalse送信
+				}
+				nowTime = (int) System.currentTimeMillis();// 現在の時間を取得
+			}while(rtm);// whileループ後判定 時間監視有効の間
+			getDOS().writeBoolean(true);// クライアントにtrue送信
+		}catch (Exception e){
+			// エラー表示
+			System.out.println(e);// エラー内容の表示
+			e.printStackTrace();// エラー原因追跡内容の表示
+		}
+	}
+
+	// 間隔 出力
+	public synchronized int getInterval(){
+		return interval;
+	}
+
+	// 間隔 入力
+	public synchronized void setInterval(int interval){
+		this.interval = interval;
+	}
+
+	// ソケット 入力
+	public synchronized void setSocket(Socket socket){
+		super.setSocket(socket);
+		try {
+			open();
+		} catch (Exception e) {
+			System.out.println(e);
+			e.printStackTrace();
+			try {
+				close();
+			} catch (Exception e1) {
+				System.out.println(e1);
+				e1.printStackTrace();
+				setDOS(null);// null値
+				setDIS(null);// null値
+			}
+		}
+	}
+
+	// 送信可能な状態をクライアントに知らせる
+	public synchronized void success(){
+		rtm = false;// 時間監視をfalseに設定
+	}
+
+}
